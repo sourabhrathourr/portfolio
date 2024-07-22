feefc336a87994d09481d79c360ed5aea654a563
Sourabh <rathoursourabh5@gmail.com>
2024-07-22 13:20:52 +0530

    style: setting up the geist fonts for the ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,8 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Hii, this is sourabh
+    <main className="flex min-h-screen flex-col antialiased items-center gap-4 p-24">
+      <h1 className="text-2xl">hi 👋🏻 this is sourabh rathour</h1>
+      <p className="">i'm a software engineer </p>
     </main>
   );
 }
